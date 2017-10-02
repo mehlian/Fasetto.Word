6fec7004394b70baeaddb5f18f7f9d0d2a51027c
mehlian <xpilarz0@gmail.com>
2017-10-02 16:15:54 +0200

    Added basic ViewModel implementation (BaseVM, RalayCommand, FodyWeaver)

---
+++ Fasetto.Word/ViewModel/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Fasetto.Word
+{
+    // A base view model that fires Property Changed events as needed
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        // The event that is fired when any child property changes its value
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            // If PropertChanged != null, -> Invoke. (? - null checker)
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Fasetto.Word/ViewModel/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Fasetto.Word
+{
+    // A basic command that runs an Action
+    public class RelayCommand : ICommand
+    {
+        private Action _action;
+
+        /// <summary>
+        /// The event thats fired when the <see cref="CanExecute(object)"/> value has changed
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        /// <summary>
+        /// A relay command can always execute
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the commands Action
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+    }
+}
